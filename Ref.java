28420697974da1e887a712799d2cbe4ae0c70008
ThatXuxe <45848414+ThatXuxe@users.noreply.github.com>
2019-03-19 19:23:50 +0530

    Update Ref.java

--- Ref.java
+++ Ref.java
@@ -4,10 +4,9 @@ import net.dv8tion.jda.core.JDA;
 
 public class Ref
 {
-    public static final String token = "NTE5MDQ3NTI0NjcwMjQyODMz.D3EtWA.ju6Ioe88rdZEOo_todxuO-Ji2pg";
+    public static final String token = "Insert-token-here";
     protected static JDA jda;
     protected static String password = "4";
     protected static String filepath = "D";
     protected static String filename = "h";
-    //"C:\Users\soham\Desktop\hello.bat"
 }
